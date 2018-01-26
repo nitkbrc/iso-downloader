23923fce6eefa842e2dc9250b96135a5b83fd75f
nitkbrc <34566580+nitkbrc@users.noreply.github.com>
2018-01-26 13:04:37 +0530

    Update wget.py

--- wget.py
+++ wget.py
@@ -10,20 +10,16 @@
 # email: 
 # Date: 14.01.2018
 
+# Algorithm:
+
+# Step 1: Downloading hash for *.iso form http://releases.ubuntu.com
+# Step 2: Compare the currently downloaded hash with privious hash to check loaclly available *.iso is updated or not.
+# Step 3: If locally available *.iso is not updated, download the new and delet the old one. 
+
 import os,re
 
-# Reading the old md5 value of .iso files
 
-path = "/home/swapnil/Desktop/U_Mirror/versions/17/MD5SUMS"
-l=[]
-with open(path,'r') as f:
-    for line in f:
-        for word in line.split():
-           l.append(word)   
-
-old = l[0]
-
-# Downloading the new md5 value of .iso files
+# Step 1: Downloading hash for *.iso from http://releases.ubuntu.com
 
 os.system("wget http://releases.ubuntu.com/17.10/MD5SUMS -P /home/swapnil/Desktop/U_Mirror/versions/17/new_MD5SUM/")
 
@@ -35,6 +31,23 @@ with open(path,'r') as f:
            l1.append(word)   
 
 newm = l1[0]
+
+
+# Step 1-1: Reading local hash for *.iso
+
+path = "/home/swapnil/Desktop/U_Mirror/versions/17/MD5SUMS"
+l=[]
+with open(path,'r') as f:
+    for line in f:
+        for word in line.split():
+           l.append(word)   
+
+old = l[0]
+
+# Step 2: Comparing hashes
+
+if old != newm and old != md :
+	os.system("wget http://releases.ubuntu.com/17.10/ubuntu-17.10-desktop-amd64.iso -P /home/swapnil/Desktop/U_Mirror/versions/")
 
 
 os.system("openssl dgst -md5 /home/swapnil/Desktop/U_Mirror/versions/17/ubuntu-17.10-desktop-amd64.iso > /home/swapnil/Desktop/U_Mirror/versions/17/MD5_cross_check.txt")
@@ -52,8 +65,8 @@ md = l2[1]
 #print newm
 #print md
 
-if old != newm and old != md :
-	os.system("wget http://releases.ubuntu.com/17.10/ubuntu-17.10-desktop-amd64.iso -P /home/swapnil/Desktop/U_Mirror/versions/")
-	os.system("wget http://releases.ubuntu.com/17.10/MD5SUMS -P /home/swapnil/Desktop/U_Mirror/versions/17/")
+# if old != newm and old != md :
+#	os.system("wget http://releases.ubuntu.com/17.10/ubuntu-17.10-desktop-amd64.iso -P /home/swapnil/Desktop/U_Mirror/versions/")
+#	os.system("wget http://releases.ubuntu.com/17.10/MD5SUMS -P /home/swapnil/Desktop/U_Mirror/versions/17/")
 
 
