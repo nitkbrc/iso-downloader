9e314c33367eed19fa7f0d1ae9490dbccf7d6677
nitkbrc <34566580+nitkbrc@users.noreply.github.com>
2018-01-26 12:40:57 +0530

    Add files via upload

---
+++ wget.py
@@ -0,0 +1,56 @@
+
+#*/10 * * * * python /etc/cron.hourly/wget.py
+
+# Phyton script to download md5 - hash based downloading of .iso of all the architectures of Ubuntu 
+# Created by:
+# CSE Dept., NITK, Surathkal
+# email: 
+# Date: 14.01.2018
+
+import os,re
+
+# Reading the old md5 value of .iso files
+
+path = "/home/swapnil/Desktop/U_Mirror/versions/17/MD5SUMS"
+l=[]
+with open(path,'r') as f:
+    for line in f:
+        for word in line.split():
+           l.append(word)   
+
+old = l[0]
+
+# Downloading the new md5 value of .iso files
+
+os.system("wget http://releases.ubuntu.com/17.10/MD5SUMS -P /home/swapnil/Desktop/U_Mirror/versions/17/new_MD5SUM/")
+
+path1 = "/home/swapnil/Desktop/U_Mirror/versions/17/new_MD5SUM/MD5SUMS"
+l1=[]
+with open(path,'r') as f:
+    for line in f:
+        for word in line.split():
+           l1.append(word)   
+
+newm = l1[0]
+
+
+os.system("openssl dgst -md5 /home/swapnil/Desktop/U_Mirror/versions/17/ubuntu-17.10-desktop-amd64.iso > /home/swapnil/Desktop/U_Mirror/versions/17/MD5_cross_check.txt")
+
+
+with open("/home/swapnil/Desktop/U_Mirror/versions/17/MD5_cross_check.txt",'r') as f:
+	md  = f.readline() 
+
+l2=[]	
+for parts in md.split("= "):
+	l2.append(parts)
+
+md = l2[1]
+#print old
+#print newm
+#print md
+
+if old != newm and old != md :
+	os.system("wget http://releases.ubuntu.com/17.10/ubuntu-17.10-desktop-amd64.iso -P /home/swapnil/Desktop/U_Mirror/versions/")
+	os.system("wget http://releases.ubuntu.com/17.10/MD5SUMS -P /home/swapnil/Desktop/U_Mirror/versions/17/")
+
+
